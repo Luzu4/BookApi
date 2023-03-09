d4dff09bea49b3073add296fc7e77591d63cb4cb
Luzu4 <maciekluz@gmail.com>
2023-03-09 14:52:11 +0100

    added features create read update delete

--- src/main/java/org/example/Controller/BookController.java
+++ src/main/java/org/example/Controller/BookController.java
@@ -1,18 +1,55 @@
 package org.example.Controller;
 
 import org.example.Model.Book;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.example.Service.MockBookService;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
 
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
+    private MockBookService mockBookService;
+
+    public BookController(MockBookService mockBookService){
+        this.mockBookService = mockBookService;
+    }
+
     @RequestMapping("/helloBook")
     public Book helloBook() {
         return new Book(1L, "9788324631766", "Thinking in Java",
                 "Bruce Eckel", "Helion", "programming");
     }
+
+    @GetMapping("")
+    public List<Book> getBooks(){
+        return mockBookService.getList();
+    }
+
+    @PostMapping("")
+    public void addNewBook(@RequestBody Map<String, String> data){
+        Book bookToAdd = new Book(data.get("isbn"),data.get("title"),data.get("author"),data.get("publisher"),data.get("type"));
+        mockBookService.addNewBook(bookToAdd);
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable long id){
+        return mockBookService.getBookById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String removeBookById(@PathVariable long id){
+        return mockBookService.removeBookById(id);
+    }
+
+    @PutMapping("")
+    public void editBookById (@RequestBody Map<String, String> data){
+        long id = Long.parseLong(data.get("id"));
+        Book bookToEdit = new Book(id,data.get("isbn"),data.get("title"),data.get("author"),data.get("publisher"),data.get("type"));
+        mockBookService.editBookById(bookToEdit);
+    }
+
 }
--- src/main/java/org/example/Service/MockBookService.java
+++ src/main/java/org/example/Service/MockBookService.java
@@ -1,4 +1,64 @@
 package org.example.Service;
 
+import org.example.Model.Book;
+import org.springframework.stereotype.Service;
+
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@Service
 public class MockBookService {
+    private List<Book> list;
+    private static Long nextId = 4L;
+
+    public MockBookService() {
+        list = new ArrayList<>();
+        list.add(new Book(1L, "9788324631766", "Thiniking	in	Java", "Bruce	Eckel", "Helion", "programming"));
+        list.add(new Book(2L, "9788324627738", "Rusz	glowa	Java.", "Sierra	Kathy,	Bates	Bert", "Helion",
+                "programming"));
+        list.add(new Book(3L, "9780130819338", "Java	2.	Podstawy", "Cay	Horstmann,	Gary	Cornell", "Helion",
+                "programming"));
+    }
+
+
+    public List<Book> getList() {
+        return list;
+    }
+
+    public void addNewBook(Book book) {
+        book.setId(nextId);
+        nextId++;
+        list.add(book);
+    }
+
+    public Book getBookById(long id) {
+        Book bookToShow = list.stream()
+                .filter(book -> book.getId().equals(id))
+                .findAny()
+                .orElse(null);
+        return bookToShow;
+    }
+
+    public String removeBookById(long id){
+        int sizeBefore = list.size();
+        list.removeIf(book ->book.getId().equals(id));
+        int sizeAfter = list.size();
+        if(sizeBefore != sizeAfter){
+            return "Failed";
+        }else{
+            return "Success";
+        }
+    }
+
+    public void editBookById(Book bookToEdit){
+        long idToEdit = bookToEdit.getId();
+
+        list.removeIf(book ->book.getId().equals(idToEdit));
+        list.add(bookToEdit);
+        list.sort(Comparator.comparingLong(Book::getId));
+    }
+
+
 }
